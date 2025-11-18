428f941166cba9b32fa949ad317c10ba8ccf5b41
Andy Louis <lowkeyalways@MacBook-Pro-de-Lowkey.local>
2025-11-18 17:18:28 +0100

    fix errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Login from './components/Login'
 import Home from './pages/Home'
-import Login from './pages/Login'
 import './App.css'
 
 function App() {
